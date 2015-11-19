fcaa3009c39561da7b4237535103949e835564c6
Søren Palmund <palmund@gmail.com>
2015-11-19 15:39:08 +0100

    Animal: more work on sensins

--- Assets/Animalz/Animal.cs
+++ Assets/Animalz/Animal.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace Assets.Animalz
 {
@@ -16,17 +16,35 @@ namespace Assets.Animalz
 
         public void Init(MindPrototype prototype)
         {
-            GameObject vision = CreateContainer( "VisionTest" );
+            Mind = gameObject.AddComponent<ConcreteMind>();
+            Mind.MyAnimal = this;
+
+            GameObject vision = CreateContainer("VisionTest");
+            var visionCollider = vision.AddComponent<SphereCollider>();
+            vision.AddComponent<Rigidbody>()
+                  .isKinematic = true;
             Eyes = vision.AddComponent<VisionSense>();
             Eyes.CreatedFrom = prototype.Eyes;
+            Eyes.Collider = visionCollider;
+            Eyes.Mind = Mind;
+            visionCollider.isTrigger = true;
+            visionCollider.radius = Eyes.Range;
 
             GameObject audio = CreateContainer("AudioTest");
+            var audioCollider = audio.AddComponent<SphereCollider>();
+            audio.AddComponent<Rigidbody>()
+                 .isKinematic = true;
             Ears = audio.AddComponent<AudiatorySense>();
             Ears.CreatedFrom = prototype.Ears;
+            Ears.Collider = audioCollider;
+            Ears.Mind = Mind;
+            audioCollider.isTrigger = true;
+            audioCollider.radius = Ears.Range;
 
             GameObject smell = CreateContainer("SmellyTest");
             Nose = smell.AddComponent<SmellySense>();
             Nose.CreatedFrom = prototype.Nose;
+
         }
 
         private GameObject CreateContainer(string objectName)
@@ -54,54 +72,128 @@ namespace Assets.Animalz
         SmellySense Nose { get; }
     }
 
-    internal class ConcreteMind : MonoBehaviour //, IConcreteMind
+    internal class ConcreteMind : MonoBehaviour
     {
-//        public AudiatorySense Ears;
-//        public VisionSense Eyes;
-//        public SmellySense Nose;
-//
-//        public ConcreteMind()
-//        {
-//            Ears = new AudiatorySense();
-//            Nose = new SmellySense();
-//            Eyes = new VisionSense();
-//        }
+        private static readonly Material CanSeeAndHear = new Material(Shader.Find("Diffuse")) { color = Color.green };
+
+        private static readonly Material CanSee = new Material(Shader.Find("Diffuse")) { color = Color.black };
+
+        private static readonly Material CanOnlyHear = new Material(Shader.Find("Diffuse")) { color = Color.yellow };
+
+        internal Animal MyAnimal;
+
+        public void Update()
+        { }
+
+        public void ReportDetectOf(Sense origin, Collider other)
+        {
+            Material m = null;
+            if (origin is VisionSense)
+            {
+                m = MyAnimal.Ears.Detected.Contains(other)
+                    ? CanSeeAndHear
+                    : CanSee;
+            }
+            else if (origin is AudiatorySense)
+            {
+                m = MyAnimal.Eyes.Detected.Contains(other)
+                    ? CanSeeAndHear
+                    : CanOnlyHear;
+            }
+            other.GetComponent<Renderer>()
+                 .material = m;
+        }
+
+        internal void ReportUndectOf(Sense origin, Collider other)
+        {
+            other.GetComponent<Renderer>()
+                    .material = new Material(Shader.Find("Diffuse"))
+                    {
+                        color = Color.white
+                    };
+        }
     }
 
-    [Serializable]
-    internal class AudiatorySense : MonoBehaviour
+    internal class AudiatorySense : Sense
     {
         internal AudioPref CreatedFrom { get; set; }
+
+        public float Range
+        {
+            // ReSharper disable once ConvertPropertyToExpressionBody
+            get { return CreatedFrom.Range; }
+        }
+
+        public void FixedUpdate()
+        {
+            if (Collider.radius != Range)
+            {
+                Collider.radius = Range;
+            }
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            if (!IsAnimalAi(other))
+            {
+                return;
+            }
+
+            if (IsSelf(other))
+            {
+                return;
+            }
+
+            var direction = other.transform.position - gameObject.transform.position;
+            var angle = Vector3.Angle(direction, transform.forward);
+            if (angle < 180)
+            {
+                ReportDetection(other);
+            }
+            else
+            {
+                ReportUndetection(other);
+            }
+        }
     }
 
-    [Serializable]
-    internal class VisionSense : MonoBehaviour
+    internal class Sense : MonoBehaviour
+    {
+        private static readonly Type AnimalType = typeof(Animal);
+
+        internal HashSet<Collider> Detected = new HashSet<Collider>();
+
+        internal SphereCollider Collider { get; set; }
+        internal ConcreteMind Mind { get; set; }
+
+        protected bool IsAnimalAi(Collider other)
+        {
+            return other.gameObject.GetComponent(AnimalType) != null;
+        }
+
+        protected bool IsSelf(Collider other)
+        {
+            var components = GetComponentInParent<Animal>();
+            var otherAnimal = other.GetComponentInParent<Animal>();
+            return components == otherAnimal;
+        }
+
+        protected void ReportUndetection(Collider other)
+        {
+            Detected.Remove(other);
+            Mind.ReportUndectOf(this, other);
+        }
+
+        protected void ReportDetection(Collider other)
+        {
+            Detected.Add(other);
+            Mind.ReportDetectOf(this, other);
+        }
+    }
+
+    internal class VisionSense : Sense
     {
         internal VisionPref CreatedFrom { get; set; }
-
-//        private readonly SphereCollider collider;
-//
-//        public VisionSense( GameObject go )
-//        {
-//            var eyes = CreateContainer(go, "EyesTest");
-//
-//            collider = eyes.AddComponent<SphereCollider>();
-//            collider.isTrigger = true;
-//            collider.radius = 0f;
-//            eyes.AddComponent(typeof(DirectionalSensor));
-//        }
-//
-//        private GameObject CreateContainer(GameObject go, string objectName)
-//        {
-//            var container = new GameObject(objectName);
-//            container.transform.parent = go.transform;
-//
-//            // Place the game object at the same position and rotation as the animal
-//            container.transform.position = go.transform.position;
-//            container.transform.rotation = go.transform.rotation;
-//            return container;
-//        }
-
 
         public float FieldOfView
         {
@@ -109,14 +201,101 @@ namespace Assets.Animalz
             get { return CreatedFrom.FieldOfView; }
         }
 
-        public void Update()
+        public float Range
         {
-            print(FieldOfView);
+            // ReSharper disable once ConvertPropertyToExpressionBody
+            get { return CreatedFrom.Range; }
+        }
+
+        public void FixedUpdate()
+        {
+            if (Collider.radius != Range)
+            {
+                Collider.radius = Range;
+            }
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (!IsAnimalAi(other))
+            {
+                return;
+            }
+
+            if (IsSelf(other))
+            {
+                return;
+            }
+
+            if (Detected.Contains(other))
+            {
+                ReportUndetection(other);
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (!IsAnimalAi(other))
+            {
+                return;
+            }
+
+            if (IsSelf(other))
+            {
+                return;
+            }
+
+            var direction = other.transform.position - gameObject.transform.position;
+            var angle = Vector3.Angle(direction, transform.forward);
+            if (angle < 0.5 * FieldOfView)
+            {
+                RaycastHit ray;
+                if (Physics.Raycast(transform.position, direction, out ray))
+                {
+                    if (ray.collider == other)
+                    {
+                        ReportDetection(other);
+                    }
+                }
+            }
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            if (!IsAnimalAi(other))
+            {
+                return;
+            }
+
+            if (IsSelf(other))
+            {
+                return;
+            }
+
+            var direction = other.transform.position - gameObject.transform.position;
+            var angle = Vector3.Angle(direction, transform.forward);
+            if (angle < 0.5 * FieldOfView)
+            {
+                RaycastHit ray;
+                if (Physics.Raycast(transform.position, direction, out ray))
+                {
+                    var otherAnimal = other.GetComponentInParent<Animal>();
+                    var collidedAnimal = ray.collider.GetComponentInParent<Animal>();
+                    if (collidedAnimal == otherAnimal)
+                    {
+                        ReportDetection(other);
+                        return;
+                    }
+                }
+            }
+            if (Detected.Contains(other))
+            {
+                ReportUndetection(other);
+            }
         }
     }
 
-    [Serializable]
-    internal class SmellySense : MonoBehaviour
+    internal class SmellySense : Sense
     {
         internal SmellyPref CreatedFrom { get; set; }
     }
